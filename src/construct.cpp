c0078b2eec5d564435c1ac1ba6a3efccda5e7ec1
xtrm <oss@xtrm.me>
2024-01-24 03:43:47 +0100

    🐛 fix(construct.cpp): add verification to input/output
    Signed-off-by: xtrm <oss@xtrm.me>

--- src/construct.cpp
+++ src/construct.cpp
@@ -17,6 +17,26 @@ int main(int argc, char** argv) {
   }
 
   std::ifstream inpfile(path);
+  if (!inpfile.is_open()) {
+	std::cout << "Could not open input file" << std::endl;
+	return 0;
+  }
+  if (!inpfile.good()) {
+	std::cout << "Input file is not good" << std::endl;
+	return 0;
+  }
+
+  std::ofstream outfile;
+  outfile.open(outpath);
+  if (!outfile.is_open()) {
+	std::cout << "Could not open output file" << std::endl;
+	return 0;
+  }
+  if (!outfile.good()) {
+	std::cout << "Output file is not good" << std::endl;
+	return 0;
+  }
+
   std::stringstream buffer;
   buffer << inpfile.rdbuf();
   std::vector<con_token*> tokens = parse_construct(buffer.str());
@@ -38,8 +58,6 @@ int main(int argc, char** argv) {
   apply_macros(tokens, empty_macros);
   linearize_tokens(tokens);
 
-  std::ofstream outfile;
-  outfile.open(outpath);
   outfile << tokens_to_nasm(tokens);
   outfile.close();
 }
