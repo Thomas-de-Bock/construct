60ce0960ce46fd597ae2d7bd25ea1eb46826feca
xtrm <oss@xtrm.me>
2024-01-24 03:42:40 +0100

    ✨ feature(construct_flags): guess the output file name if it's not provided
    Signed-off-by: xtrm <oss@xtrm.me>

--- src/construct_flags.cpp
+++ src/construct_flags.cpp
@@ -24,6 +24,28 @@ int set_bitwidth(char* argv) {
   return -1;
 }
 
+/**
+ * Returns the output path for a given input path.
+ *
+ * If the input path has no extension, ".construct.asm" is appended.
+ * If the input path has an extension:
+ * - If the extension is ".asm", it is replaced with ".construct.asm"
+ *   (e.g. "foo.asm" -> "foo.construct.asm")
+ * - Otherwise, the extension is removed and ".construct.asm" is appended
+ *   (e.g. "foo.con" -> "foo.asm")
+ */
+string get_outpath(string path) {
+	if (path.find(".") == string::npos) {
+		return path + ".construct.asm";
+	}
+	int last_dot = path.find_last_of(".");
+	std::string extension = path.substr(last_dot);
+	if (extension == ".asm") {
+		return path.substr(0, last_dot) + ".construct.asm";
+	}
+	return path.substr(0, last_dot) + ".asm";
+}
+
 int handle_flags(int argc, char** argv, string* path, string* outpath) {
   bool bitwidth_set = false;
   bool path_set = false;
@@ -60,8 +82,8 @@ int handle_flags(int argc, char** argv, string* path, string* outpath) {
     return -1;
   }
   if(!outpath_set) {
-    cout << "flag -o (output file) not set" << endl;
-    return -1;
+	(*outpath) = get_outpath((*path));
+	cout << "flag -o (output file) not set, using " << (*outpath) << endl;
   }
   return 0;
 }
