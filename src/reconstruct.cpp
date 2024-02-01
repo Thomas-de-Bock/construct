6249b02b892edbd7a448ae7f3840923e5879d96b
NoneSince <abofoulm@gmail.com>
2024-02-01 22:55:42 +0200

    passed arguments to funcalls and syscallscorrectly

--- src/reconstruct.cpp
+++ src/reconstruct.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+#define min(a,b) ((a)<=(b) ? (a) : (b))
+
 int if_amnt = 0;
 int while_amnt = 0;
 CON_BITWIDTH bitwidth = BIT64;
@@ -142,6 +144,22 @@ static string reg_to_str(uint8_t call_num, CON_BITWIDTH bitwidth)
   throw invalid_argument("Invalid bitwidth or call_num: bitwidth="+to_string(static_cast<int>(bitwidth))
                          +" call_num="+to_string(static_cast<int>(call_num)));
 }
+static uint8_t str_to_reg(string reg_name)
+{
+  if (reg_name=="dil" ||reg_name=="di" || reg_name=="edi" || reg_name=="rdi")
+    return 0;
+  if (reg_name=="sil" ||reg_name=="si" || reg_name=="esi" || reg_name=="rsi")
+    return 1;
+  if (reg_name=="dl" || reg_name=="dx" || reg_name=="edx" || reg_name=="rdx")
+    return 2;
+  if (reg_name=="cl" || reg_name=="cx" || reg_name=="ecx" || reg_name=="rcx")
+    return 3;
+  if (reg_name=="r8b" || reg_name=="r8w" || reg_name=="r8d" || reg_name=="r8")
+    return 4;
+  if (reg_name=="r9b" || reg_name=="r9w" || reg_name=="r9d" || reg_name=="r9")
+    return 5;
+  return 6;
+}
 static void apply_macro_to_token(con_token& token, vector<con_macro> macros)
 {
   if (token.tok_type != WHILE && token.tok_type != IF && token.tok_type != CMD) {
@@ -203,18 +221,97 @@ static void apply_macro_to_token(con_token& token, vector<con_macro> macros)
     }
   }
 }
-static vector<con_token*> push_args(vector<string>& args)
+
+static vector<con_token*> push_args(vector<string>& args, CON_BITWIDTH bitwidth)
 {
   vector<con_token*> arg_tokens;
-  for (size_t j = 0; j < args.size(); j++) {
+
+  // stack args;
+  for (size_t i = 6; i < args.size() ; ++i) {
+    size_t i_rev = args.size()+5 - i;
     con_token* arg_tok = new con_token();
     arg_tok->tok_type = CMD;
     con_cmd* arg_cmd = new con_cmd();
     arg_tok->tok_cmd = arg_cmd;
-    arg_cmd->command = "mov";
-    arg_cmd->arg1 = reg_to_str(j, bitwidth);
-    arg_cmd->arg2 = args[j];
+    arg_cmd->command = "pushq"; // bitwidth
+    arg_cmd->arg1 = args[i_rev];
     arg_tokens.push_back(arg_tok);
+  }
+
+  // register args;
+  size_t reg_args_size = min(args.size(),6);
+  uint8_t first_read[7] = {6,6,6,6,6,6,6}; // cell 6 is garbage to hold not special-regs
+  for (size_t i = 0; i < reg_args_size; ++i) {
+    uint8_t reg_num = str_to_reg(args[i]);
+    first_read[reg_num] = min(first_read[reg_num],i);
+  }
+  // sort regs by first-read
+  uint8_t read_order[6] = {6,6,6,6,6,6};
+  for (size_t fr = 0; fr < reg_args_size; ++fr) {
+    for (size_t reg = 0; reg < reg_args_size; ++reg) {
+      if ((fr == first_read[reg]) && (first_read[reg] > reg)) { //next in turn and will be pushed to stack
+        read_order[fr]=reg;
+      }
+    }
+  }
+  // push reversed to pop order
+  for (size_t fr = 0; fr < 6; ++fr) {
+    size_t fr_rev = 5 - fr; // reverse the order
+    if (read_order[fr_rev] != 6) { // there is a regester first read i arg number fr, and will be deleted before
+      con_token* arg_tok = new con_token();
+      arg_tok->tok_type = CMD;
+      con_cmd* arg_cmd = new con_cmd();
+      arg_tok->tok_cmd = arg_cmd;
+      arg_cmd->command = "push";
+      arg_cmd->arg1 = reg_to_str(read_order[fr_rev], bitwidth);
+      arg_tokens.push_back(arg_tok);
+    }
+  }
+  // set each arg and track values places
+  uint8_t current_val_place[6] = {0,1,2,3,4,5}; // 6 means stack
+  for (size_t reg = 0; reg < reg_args_size; ++reg) {
+    if (first_read[reg] > reg) {
+      current_val_place[reg] = 6;
+    }
+  }
+  for (size_t i = 0; i < reg_args_size; i++) {
+    con_token* arg_tok = new con_token();
+    arg_tok->tok_type = CMD;
+    con_cmd* arg_cmd = new con_cmd();
+    arg_tok->tok_cmd = arg_cmd;
+    uint8_t wanted_reg = str_to_reg(args[i]);
+    if (wanted_reg==6) {
+      arg_cmd->command = "mov";
+      arg_cmd->arg1 = reg_to_str(i, bitwidth);
+      arg_cmd->arg2 = args[i];
+      // if regi was read before, then current_val_place[i] is a previous register (correct)
+      // if regi isn't read yet, then current_val_place[i] is stack (correct)
+    } else {
+      if (current_val_place[wanted_reg] == 6) {
+        arg_cmd->command = "pop";
+        arg_cmd->arg1 = reg_to_str(i, bitwidth);
+        current_val_place[wanted_reg] = i; // wanted_reg moved from stack to regi
+      } else {
+        if (i != current_val_place[wanted_reg]) {
+          arg_cmd->command = "mov";
+          arg_cmd->arg1 = reg_to_str(i, bitwidth);
+          arg_cmd->arg2 = reg_to_str(current_val_place[wanted_reg], bitwidth);
+          // if regi was read before, then current_val_place[i] is a previous register (correct)
+          // if regi isn't read yet, then current_val_place[i] is stack (correct)
+          current_val_place[wanted_reg] = min(current_val_place[wanted_reg],i);
+        } else {
+          arg_cmd->command = "nop";
+        }
+      }
+    }
+    if (arg_cmd->command == "nop") {
+      delete arg_cmd;
+      arg_cmd = nullptr;
+      delete arg_tok;
+      arg_tok = nullptr;
+    } else {
+      arg_tokens.push_back(arg_tok);
+    }
   }
   return arg_tokens;
 }
@@ -377,7 +474,7 @@ void apply_funcalls(std::vector<con_token*>& tokens)
     if (tokens[i]->tok_type != FUNCALL) {
       continue;
     }
-    vector<con_token*> arg_tokens = push_args(tokens[i]->tok_funcall->arguments);
+    vector<con_token*> arg_tokens = push_args(tokens[i]->tok_funcall->arguments, bitwidth);
     con_token* call_tok = new con_token();
     call_tok->tok_type = CMD;
     con_cmd* call_cmd = new con_cmd();
@@ -396,7 +493,7 @@ void apply_syscalls(std::vector<con_token*>& tokens)
     if (tokens[i]->tok_type != SYSCALL) {
       continue;
     }
-    vector<con_token*> arg_tokens = push_args(tokens[i]->tok_syscall->arguments);
+    vector<con_token*> arg_tokens = push_args(tokens[i]->tok_syscall->arguments, bitwidth);
     con_token* call_tok1 = new con_token();
     call_tok1->tok_type = CMD;
     con_cmd* call_cmd = new con_cmd();
